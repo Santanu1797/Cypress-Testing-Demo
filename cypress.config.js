4ad1daa281651eff9a636dc787c1ba5bac53a95e
Santanu1797 <santanu.das@capgemini.com>
2023-08-09 18:04:31 +0530

    some changes made

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config){
+      baseUrl: 'http://localhost:8080',
       allureWriter(on, config);
             return config;
       
